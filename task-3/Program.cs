601abdfdc92c51e6f66a94397da5910365c50519
Olgerd425 <78820918+Olgerd425@users.noreply.github.com>
2021-02-17 20:04:08 +0200

    task-3

---
+++ task-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            string numer = Console.ReadLine();
+            if (numer.Contains('5')) {
+                string a = numer.Trim('5');
+                int b =Convert.ToInt32(a);
+                Console.Write($"{b*(++b)}"+"25");
+            }
+        }
+    }
+}
